17933e9a357f96a136194e7af7b066a8cc6cbb04
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-01-03 01:51:28 +0330

    Moved tests js code into app-tests.js

--- js/app.js
+++ js/app.js
@@ -58,63 +58,6 @@ var People = Backbone.Collection.extend({
 	model: Person
 });
 
-/**
- * Tests
- */
-var Test1View = Backbone.View.extend({
-    el: '#test-1',
-    $el: $('#test-1'),
-    template: _.template($('#test-1-tpl').html()),
-
-    initialize: function() {
-        this.render();
-    },
-
-    render: function () {
-        this.$el.html(this.template());
-    }
-});
-
-var Test2Model = Backbone.Model.extend({
-    defaults: {
-        name: 'User'
-    }
-});
-
-var Test2View = Backbone.View.extend({
-    el: '#test-2',
-    template: _.template($('#test-2-tpl').html()),
-    model: new Test2Model(),
-
-    events: {
-        'click button.btn': 'click',
-        'keyup input.username': 'nameChange'
-    },
-
-    initialize: function () {
-        this.$el = $(this.el);
-        this.render();
-    },
-
-    nameChange: function (event) {
-        var val = event.target.value;
-
-        // If textbox for name is empty use default value
-        if (val.length == 0) {
-            this.model.set('name', this.model.defaults.name);
-        } else {
-            this.model.set('name', val);
-        }
-    },
-
-    click: function () {
-        this.render();
-    },
-
-    render: function () {
-       this.$el.html(this.template(this.model.attributes));
-    }
-})
 
 t1v = new Test1View();
 t2v = new Test2View();
