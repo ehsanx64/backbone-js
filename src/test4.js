8debb6e72375c89359ebd69fed5d1f17ff650b88
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-01-04 15:37:00 +0330

    Added some test code using the string substitution inside another string (template)

--- src/test4.js
+++ src/test4.js
@@ -4,9 +4,20 @@ var test4Content = " \
 is about how to use Backbone.js powered UI components.\
 </p>\
 ";
+
+const test4Content2meta = {
+    title: 'Test #4 - II',
+    description: 'is the second version of the Backbone.js powered UI components demo.'
+}
+
+const test4Content2 = `
+    <p><em><strong>${test4Content2meta.title}</strong></em>&nbsp; ${test4Content2meta.description}</p>
+`;
+
+
 testbox4 = new TestBox({
     'el': '#test-4',
     'title': "Test #4",
-    'content': test4Content,
+    'content': test4Content2,
     'image': 'images/service-4.jpg'
 });
