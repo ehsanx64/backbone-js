00407b80fb12aab21c17ca09a60848ac03a1eef7
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-02-14 16:18:17 +0330

    Added some simple tests to ts1

--- src/ts1.js
+++ src/ts1.js
@@ -1,6 +1,34 @@
+var Shop = Backbone.Model.extend({
+   defaults: {
+       brand: '',
+       type: '',
+       price: 900,
+       stock: 0
+   }
+});
 
 (function ($) {
     $(function () {
         console.log('Running onready callback from ts1');
+
+        // Let's create two shops first
+        var firstShop = new Shop({
+            brand: 'Rolex',
+            type: 'Wrist Watch',
+            price: 900,
+            stock: 8
+        });
+
+        var secondShop = new Shop({
+            brand: 'Michael Korrs',
+            type: 'Wrist Watch',
+            price: 400,
+            stock: 16
+        });
+
+
+        // Show which brands each shop has
+        console.log('First shop sell: ' + firstShop.get('brand'));
+        console.log('Second shop sell: ' + secondShop.get('brand'));
     });
 })(jQuery);
