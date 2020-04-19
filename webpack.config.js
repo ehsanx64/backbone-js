d959a3d7ccb106cd68455f39688d09f99923e0d6
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-04-19 23:07:44 +0430

    Added development mode to webpack.config.js; Updated api url to new one

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     // This is the entry script
     entry: './src/index.js',
+    mode: 'development',
     // Where output (bundle) file is located
     output: {
         filename: 'app.js',
